db4dcb585e400fc2c276834491722e1aa2c2541a
Hozan Baydu <hozan.baydu3447@gmail.com>
2022-12-12 13:37:47 +0300

    adapterin oluşturulup verilerin kullanıcıya gösterilmesi işlemleri

---
+++ app/src/main/java/com/hozanbaydu/scorp/adapter/MainAdapter.kt
@@ -0,0 +1,98 @@
+package com.hozanbaydu.scorp.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.RequestManager
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.hozanbaydu.scorp.R
+import com.hozanbaydu.scorp.databinding.MainRecyclerrowBinding
+import com.hozanbaydu.scorp.model.ScorpModel
+import com.hozanbaydu.scorp.view.MainFragmentDirections
+import kotlinx.coroutines.NonDisposableHandle.parent
+import kotlinx.coroutines.coroutineScope
+import javax.inject.Inject
+
+class MainAdapter @Inject constructor(
+
+    val glide : RequestManager
+
+): RecyclerView.Adapter<MainAdapter.PostHolder>() {
+    class PostHolder(val binding:MainRecyclerrowBinding): RecyclerView.ViewHolder(binding.root){
+
+    }
+
+    private lateinit var auth: FirebaseAuth
+
+
+    var postList=ArrayList<ScorpModel>()
+
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PostHolder {
+        val binding=MainRecyclerrowBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return PostHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: PostHolder, position: Int) {
+
+        auth = Firebase.auth
+        val currentUsers=auth.currentUser
+
+
+        holder.binding.textView.text=postList.get(position).comment
+        holder.binding.currentName.text=postList.get(position).currentName
+        holder.binding.tagName.text=postList.get(position).tagName
+        var url=postList.get(position).dowlandUrl
+        var tagUrl=postList.get(position).tagUrl
+
+
+        glide.load(url).into(holder.binding.userImageview)
+        glide.load(tagUrl).into(holder.binding.tagImageview)
+        println(currentUsers?.displayName.toString())
+
+
+
+            holder.binding.tagImageview.setOnClickListener {
+
+                if (currentUsers?.displayName==holder.binding.tagName.text.toString()) {
+                    holder.binding.tagImageview.findNavController().navigate(
+                        MainFragmentDirections.actionMainFragmentToUploadTagFragment(
+                            postList.get(position).documentId
+                        )
+                    )
+                }else{
+
+                    var tageVoteNumber=holder.binding.tagVote.text.toString().toInt()+1
+
+                    holder.binding.tagVote.text=tageVoteNumber.toString()
+
+
+
+                }
+            }
+
+        holder.binding.userImageview.setOnClickListener {
+            var currentVoteNumber=holder.binding.currentVote.text.toString().toInt()+1
+
+            holder.binding.currentVote.text=currentVoteNumber.toString()
+        }
+
+
+
+
+
+
+
+    }
+
+    override fun getItemCount(): Int {
+        return postList.size
+    }
+}
---
+++ app/src/main/java/com/hozanbaydu/scorp/viewmodel/ScorpViewmodel.kt
@@ -0,0 +1,54 @@
+package com.hozanbaydu.scorp.viewmodel
+
+import android.content.Context
+import android.net.Uri
+import android.widget.EditText
+import androidx.lifecycle.ViewModel
+import com.hozanbaydu.scorp.adapter.MainAdapter
+import com.hozanbaydu.scorp.model.ScorpModel
+import com.hozanbaydu.scorp.service.Firebase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+
+@HiltViewModel
+class ScorpViewmodel @Inject constructor(
+    private val firebase:Firebase,
+    private val adapter: MainAdapter
+
+):ViewModel() {
+
+    fun updateFireebase(context: Context, selectedPicture: Uri, tagText: EditText, currentName:String,commentText: EditText){
+
+        firebase.upload(context,selectedPicture,tagText,currentName,commentText)
+    }
+
+    var currentUserName=String()
+
+
+
+    var postArrayList=firebase.postArrayList
+
+    fun update(id:String,selectedPicture: Uri){
+        firebase.updateData(id,selectedPicture)
+    }
+
+    fun uploadVote(id: String,vote:String){
+        firebase.uploadVote(id,vote)
+    }
+
+
+
+
+
+
+
+
+
+    fun getData(context: Context){
+        firebase.getdata(context)
+    }
+
+
+
+}
