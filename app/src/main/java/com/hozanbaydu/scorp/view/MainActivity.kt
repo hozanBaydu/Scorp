7a43d104425931fa67b37052b104c215905f5f6e
Hozan Baydu <hozan.baydu3447@gmail.com>
2022-12-12 13:33:25 +0300

    fragment factory

---
+++ app/src/main/java/com/hozanbaydu/scorp/view/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.hozanbaydu.scorp.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.hozanbaydu.scorp.R
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
+
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var fragmentFactory: FragmentFactory
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        supportFragmentManager.fragmentFactory=fragmentFactory
+        setContentView(R.layout.activity_main)
+
+
+        supportActionBar?.hide()
+    }
+}
