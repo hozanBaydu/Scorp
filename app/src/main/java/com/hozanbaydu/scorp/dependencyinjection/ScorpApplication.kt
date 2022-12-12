a3cf627609b313deed5913b4abecf6b6d4e7a5ea
Hozan Baydu <hozan.baydu3447@gmail.com>
2022-12-12 13:36:44 +0300

    modelin oluşturulması

---
+++ app/src/main/java/com/hozanbaydu/scorp/dependencyinjection/ScorpApplication.kt
@@ -0,0 +1,8 @@
+package com.hozanbaydu.scorp.dependencyinjection
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ScorpApplication:Application() {
+}
---
+++ app/src/main/java/com/hozanbaydu/scorp/model/ScorpModel.kt
@@ -0,0 +1,4 @@
+package com.hozanbaydu.scorp.model
+
+data class ScorpModel (val userEmail:String?,val comment:String?,val currentName:String?
+,val tagName:String?,val dowlandUrl:String?,val documentId:String,val tagUrl:String?)
